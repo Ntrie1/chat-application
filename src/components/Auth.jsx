cdf89af632471aaac0d8de4f4c0b8acb725d8a03
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2025-03-17 21:02:37 +0200

    cookie added

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { auth, provider } from "../firebase-config.js";
-import { signInWithPopup } from "firebase/auth"
+import { signInWithPopup } from "firebase/auth";
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
 
 export default function Auth() {
     const signInWithGoogle = async () => {
-        await signInWithPopup(auth, provider);
+      try {
+        const result = await signInWithPopup(auth, provider);
+        cookies.set('auth-token', result.user.refreshToken);
+      } catch (error) {
+        console.error(error);
+      }
+       
+        
     }
   return (
     <>
