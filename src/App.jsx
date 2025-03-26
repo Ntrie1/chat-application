94f0465dca9ca26fe12f58fda22aa180c5d674cb
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2025-03-26 16:23:43 +0200

    signout added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import Auth from './components/Auth'
 import Cookies from 'universal-cookie';
 import Chat from './components/Chat';
+import { signOut } from 'firebase/auth';
+import { auth } from './firebase-config'
 
 const cookies = new Cookies();
 
@@ -11,20 +13,42 @@ function App() {
   const [room, setRoom] = useState(null);
   const roomInputRef = useRef(null);
 
-  if(!isAuth){
+  const signUserOut = async () => {
+    await signOut(auth);
+    cookies.remove("auth-token");
+    setIsAuth(false);
+    setRoom(null);
+  }
+
+  if (!isAuth) {
     return (
       <>
-        <Auth setIsAuth={setIsAuth}/>
+        <Auth setIsAuth={setIsAuth} />
       </>
     )
   }
 
-  return <div>{room ? <div> <Chat room={room} /></div> : <div className='room'> 
-  <label> Enter room name</label>
-  <input ref={roomInputRef} />
-  <button onClick={() => setRoom(roomInputRef.current.value)}>enter chat</button>
-  </div> }</div>
- 
+  return (
+
+    <>
+      {room ? (<div> <Chat room={room} /></div>) : (<div className='room'>
+        <label> Enter room name</label>
+        <input ref={roomInputRef} />
+        <button onClick={() => setRoom(roomInputRef.current.value)}>
+          enter chat
+        </button>
+      </div>
+      )}
+
+      <div className='sign-out'>
+        <button onClick={signUserOut}>Sign Out</button>
+      </div>
+
+
+    </>
+
+  );
+
 }
 
 export default App
